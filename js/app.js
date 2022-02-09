1fec526702a09e771671db7b8563637f08cdd5b9
hbarrons <hbarrons@gmail.com>
2022-02-09 11:57:59 -0500

    add dice roll generators

--- js/app.js
+++ js/app.js
@@ -1,4 +1,34 @@
+/*-------------------------------- Notes --------------------------------*/
+//Add dropdown from bootstrap for player selection
+
+//Add modal from bootsrap to confirm player category selection
+
+
 /*-------------------------------- Constants --------------------------------*/
+const diceRollOne = function () {
+  return (Math.floor(Math.random() * 6 + 1))
+}
+
+const diceRollTwo = function () {
+  return (Math.floor(Math.random() * 6 + 1))
+}
+
+const diceRollThree = function () {
+  return (Math.floor(Math.random() * 6 + 1))
+}
+
+const diceRollFour = function () {
+  return (Math.floor(Math.random() * 6 + 1))
+}
+
+const diceRollFive = function () {
+  return (Math.floor(Math.random() * 6 + 1))
+}
+
+const diceRollSix = function () {
+  return (Math.floor(Math.random() * 6 + 1))
+}
+
 
 
 
